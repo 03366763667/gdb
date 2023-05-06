11337f60806966ec282f5566f3c414ccc8fc707e
Dawood Hashmi <dawoodhashmi66@gmail.com>
2023-05-06 19:43:24 +0500

    new layout start

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -4,8 +4,11 @@
 <title>@yield('title')</title>
 <!-- Favicon -->
 <link rel="icon" type="image/png" href="images/favicon.png">
-<!-- Web Font -->
-<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+<!--  google fonts  -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
 
 <!-- StyleSheet -->
 <link rel="manifest" href="/manifest.json">
@@ -35,29 +38,8 @@
 <!-- Eshop StyleSheet -->
 <link rel="stylesheet" href="{{asset('frontend/css/reset.css')}}">
 <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
-<link rel="stylesheet" href="{{asset('frontend/css/frontend-custom.css')}}">
+{{--<link rel="stylesheet" href="{{asset('frontend/css/frontend-custom.css')}}">--}}
 <link rel="stylesheet" href="{{asset('frontend/css/responsive.css')}}">
 {{--<script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5f2e5abf393162001291e431&product=inline-share-buttons' async='async'></script>--}}
-<style>
-    /* Multilevel dropdown */
-    .dropdown-submenu {
-    position: relative;
-    }
 
-    .dropdown-submenu>a:after {
-    content: "\f0da";
-    float: right;
-    border: none;
-    font-family: 'FontAwesome';
-    }
-
-    .dropdown-submenu>.dropdown-menu {
-    top: 0;
-    left: 100%;
-    margin-top: 0px;
-    margin-left: 0px;
-    }
-
-    /*
-</style>
 @stack('styles')
